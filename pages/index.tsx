3280220871b856e00f4fe85d9b4de3e844537132
Russell Elliott <russell@quadsquad.net>
2022-07-20 10:19:56 -0700

    loader script for map complete; added APIs

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,5 +2,12 @@ import { useLoadScript } from "@react-google-maps/api";
 import Map from "../components/map";
 
 export default function Home() {
-  return <div>Map</div>;
+  const {isLoaded} = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY, //get the API key from .env.local
+    libraries: ["places"], //Google Places used to find locations
+  });
+
+  //check if the script above has finished loading
+  if(!isLoaded) return <div>Loading...</div> //display when the map is loading
+  return <div>Map</div>; //when done loading, display the map
 }
