881c054f0dbba55d3c877a232d3aa477d75822e4
Russell Elliott <russell@quadsquad.net>
2022-07-20 12:27:57 -0700

    able to get directions to specific marker

--- components/map.tsx
+++ components/map.tsx
@@ -9,83 +9,113 @@ import {
 import Places from "./places";
 import Distance from "./distance";
 
-//shortcuts for stuff we are using
 type LatLngLiteral = google.maps.LatLngLiteral;
 type DirectionsResult = google.maps.DirectionsResult;
 type MapOptions = google.maps.MapOptions;
 
-//render the map
 export default function Map() {
-  //set up the location of the office
   const [office, setOffice] = useState<LatLngLiteral>();
+  const [directions, setDirections] = useState<DirectionsResult>();
+  const mapRef = useRef<GoogleMap>();
+  const center = useMemo<LatLngLiteral>(
+    () => ({ lat: 43.45, lng: -80.49 }),
+    []
+  );
+  const options = useMemo<MapOptions>(
+    () => ({
+      disableDefaultUI: true,
+      clickableIcons: false,
+    }),
+    []
+  );
+  const onLoad = useCallback((map) => (mapRef.current = map), []);
+  const houses = useMemo(() => generateHouses(center), [center]);
 
-  //make a mapRef
-  const mapRef = useRef<GoogleMap>(); //give it a type using "<>", tell it's an instance of GoogleMap
+  const fetchDirections = (house: LatLngLiteral) => {
+    if (!office) return;
 
-  //use same instance of the object to avoid centering issues
-  //useMemo; use the same object unless one of the parameters has changed
-  //no dependencies (2nd argument); get exact same object every time
-  const center = useMemo<LatLngLiteral>(() => ({lat:43, lng: -80}), []);
+    const service = new google.maps.DirectionsService();
+    service.route(
+      {
+        origin: house,
+        destination: office,
+        travelMode: google.maps.TravelMode.DRIVING,
+      },
+      (result, status) => {
+        if (status === "OK" && result) {
+          setDirections(result);
+        }
+      }
+    );
+  };
 
-  //implement options for our map
-  const options = useMemo<MapOptions>(() => ({
-    disableDefaultUI: true,
-    clickableIcons: false
-  }), []);
+  return (
+    <div className="container">
+      <div className="controls">
+        <h1>Commute?</h1>
+        <Places
+          setOffice={(position) => {
+            setOffice(position);
+            mapRef.current?.panTo(position);
+          }}
+        />
+        {!office && <p>Enter the address of your office.</p>}
+        {directions && <Distance leg={directions.routes[0].legs[0]} />}
+      </div>
+      <div className="map">
+        <GoogleMap
+          zoom={10}
+          center={center}
+          mapContainerClassName="map-container"
+          options={options}
+          onLoad={onLoad}
+        >
+          {directions && (
+            <DirectionsRenderer
+              directions={directions}
+              options={{
+                polylineOptions: {
+                  zIndex: 50,
+                  strokeColor: "#1976D2",
+                  strokeWeight: 5,
+                },
+              }}
+            />
+          )}
 
-  //populate the mapRef
-  const onLoad = useCallback(map => (mapRef.current = map), []);
-
-  //generate the houses from center location
-  //houses regenerate when the center location changes, like when the user moves across the map
-  const houses = useMemo(() => generateHouses(center), [center])
-  return <div className="container">
-    <div className="controls">
-      <h1>Commute?</h1>
-      <Places setOffice={(position) => {
-        setOffice(position);
-        mapRef.current?.panTo(position);
-      }}/>
-    </div>
-    <div className="map">
-      <GoogleMap
-        zoom={10}
-        center={center}
-        mapContainerClassName="map-container"
-        options={options}
-        onLoad={onLoad}
-      >
-        {office && (
-        <>
-          <Marker 
-            position={office}
-            icon={"https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"}
-          />
-
-          <MarkerClusterer>
-            {clusterer =>
-              houses.map((house) => (
+          {office && (
+            <>
               <Marker
-                key={house.lat}
-                position={house}
-                clusterer={clusterer}
+                position={office}
+                icon="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
               />
-              )) 
-            }
-          </MarkerClusterer>
 
-          <Circle center = {office} radius={15000} options={closeOptions}/>
-          <Circle center = {office} radius={30000} options={middleOptions}/>
-          <Circle center = {office} radius={45000} options={farOptions}/>
-        </>
-        )}
-      </GoogleMap>
+              <MarkerClusterer>
+                {(clusterer) =>
+                  houses.map((house) => (
+                    <Marker
+                      key={house.lat}
+                      position={house}
+                      clusterer={clusterer}
+                      onClick={() => {
+                        fetchDirections(house);
+                      }}
+                    />
+                  ))
+                }
+              </MarkerClusterer>
+
+              <Circle center={office} radius={15000} options={closeOptions} />
+              <Circle center={office} radius={30000} options={middleOptions} />
+              <Circle center={office} radius={45000} options={farOptions} />
+            </>
+          )}
+        </GoogleMap>
+      </div>
     </div>
-
-  </div>;
+  );
 }
 
-//options fir displaying circles to indicate commuting distances
 const defaultOptions = {
   strokeOpacity: 0.5,
   strokeWeight: 2,
@@ -116,7 +146,6 @@ const farOptions = {
   fillColor: "#FF5252",
 };
 
-//generate houses at random locations
 const generateHouses = (position: LatLngLiteral) => {
   const _houses: Array<LatLngLiteral> = [];
   for (let i = 0; i < 100; i++) {
--- components/places.tsx
+++ components/places.tsx
@@ -17,37 +17,39 @@ type PlacesProps = {
 
 export default function Places({ setOffice }: PlacesProps) {
   const {
-    ready, 
-    value, 
-    setValue, 
-    suggestions:{status, data}, 
-    clearSuggestions
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
   } = usePlacesAutocomplete();
 
-  //function called when user selects location
-  const handleSelect = async(val: string) => {
-    setValue(val, false); //get value, don't fetch data
-    clearSuggestions(); //clear suggestions list
+  const handleSelect = async (val: string) => {
+    setValue(val, false);
+    clearSuggestions();
 
-    const results = await getGeocode({address: val}); //result
-    const {lat, lng} = await getLatLng(results[0]); //pass in first result
-    setOffice({lat, lng}); //set office location
-  }
+    const results = await getGeocode({ address: val });
+    const { lat, lng } = await getLatLng(results[0]);
+    setOffice({ lat, lng });
+  };
 
-  return <Combobox onSelect={handleSelect}>
-    <ComboboxInput value={value}
-    onChange={e => setValue(e.target.value)}
-    disabled={!ready}
-    className="combobox-input"
-    placeholder="Search office address"
-    />
-    <ComboboxPopover>
-      <ComboboxList>
+  return (
+    <Combobox onSelect={handleSelect}>
+      <ComboboxInput
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        disabled={!ready}
+        className="combobox-input"
+        placeholder="Search office address"
+      />
+      <ComboboxPopover>
+        <ComboboxList>
           {status === "OK" &&
             data.map(({ place_id, description }) => (
               <ComboboxOption key={place_id} value={description} />
             ))}
         </ComboboxList>
-    </ComboboxPopover>
-  </Combobox>;
+      </ComboboxPopover>
+    </Combobox>
+  );
 }
