7164fe659028727dc1e5eb4fe09534aa8196f08c
Russell Elliott <russell@quadsquad.net>
2022-07-20 11:42:55 -0700

    suggestions box implemented

--- components/places.tsx
+++ components/places.tsx
@@ -26,6 +26,16 @@ export default function Places({ setOffice }: PlacesProps) {
 
   return <Combobox onSelect={() => {}}>
     <ComboboxInput value={value} onChange={e => setValue(e.target.value)}
-    className="combobox-input"/>
+    className="combobox-input"
+    placeholder="Search office address"
+    />
+    <ComboboxPopover>
+      <ComboboxList>
+          {status === "OK" &&
+            data.map(({ place_id, description }) => (
+              <ComboboxOption key={place_id} value={description} />
+            ))}
+        </ComboboxList>
+    </ComboboxPopover>
   </Combobox>;
 }
